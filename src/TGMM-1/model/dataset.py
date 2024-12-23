167cd75d18b810dc3450107bfe26a4c393da29c7
Luis Carretero Lopez <luis.carretero@gmx.de>
2024-12-23 01:59:10 +0100

    Added GraphMSO dataset support

--- src/TGMM-1/model/dataset.py
+++ src/TGMM-1/model/dataset.py
@@ -7,9 +7,10 @@ import torch
 import os
 from torch.utils.data import Subset
 from model.transform import GraphPartitionTransform, PositionalEncodingTransform
+from hdtts_dataset_creation import GraphMSO, EngRad, PvUS
 
 
-class DynamicNodeDataset(Dataset):
+class DynamicNodeFeatureDataset(Dataset):
     def __init__(self, x, y):
 
         # Shape should be ()
@@ -21,6 +22,12 @@ class DynamicNodeDataset(Dataset):
     
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
+    
+    def __str__(self):
+        return self.__repr__()
+    
+    def __repr__(self):
+        return f"DynamicNodeDataset(x={self.x.shape}, y={self.y.shape})"
 
 
 class StaticGraphTopologyData(object):
@@ -32,6 +39,56 @@ class StaticGraphTopologyData(object):
         self.edge_weight = torch.tensor(edge_weight)
         self.num_nodes = num_nodes
 
+    def __repr__(self):
+        return f"StaticGraphTopologyData(edge_index={self.edge_index.shape}, edge_weight={self.edge_weight.shape}, num_nodes={self.num_nodes})"
+    
+    def __str__(self):
+        return self.__repr__()
+    
+def create_sliding_window_dataset(dataset, window, delay, horizon, stride):
+    data = dataset.target.squeeze()
+    assert data.ndim == 2  # (n_timesteps, n_nodes)
+    steps = data.shape[0]
+
+    x_idx = np.arange(window)[np.newaxis, :] + np.arange(steps-window-delay-horizon, step=stride)[:, np.newaxis]  # (batch_size, window)
+    y_idx = np.arange(window+delay+1, window+delay+horizon+1)[np.newaxis, :] + np.arange(steps-window-delay-horizon, step=stride)[:, np.newaxis]  # (batch_size, horizon)
+    assert x_idx.shape[0] == y_idx.shape[0]
+
+    x, y = data[x_idx], data[y_idx]
+    return x, y
+
+def get_data_raw(cfg, root='/data'):
+    if cfg.dataset.name == 'GraphMSO':
+        dataset = GraphMSO(root=os.path.join(root, 'GraphMSO'))
+
+        # Dynamic; (n_timesteps, n_nodes, n_features)
+        x, y = create_sliding_window_dataset(dataset, cfg.dataset.window, cfg.dataset.delay, cfg.dataset.horizon, cfg.dataset.stride)
+
+        # Static
+        adj = dataset.get_connectivity({'include_self': False, 'layout': 'edge_index'})
+        edge_index = adj[0]
+        edge_weight = adj[1]
+        n_nodes = dataset.target.shape[1]
+
+    elif cfg.dataset.name == 'METRLA':
+        assert cfg.dataset.delay == 0, "Delay must be 0 for METRLA"
+        assert cfg.dataset.stride == 1, "Stride must be 1 for METRLA"
+
+        ssl._create_default_https_context = ssl._create_unverified_context  # Fix for SSL verification error
+        loader = METRLADatasetLoader(raw_data_dir=os.path.join(root, 'METRLA'))
+        loader._get_edges_and_weights()
+        loader._generate_task(cfg.dataset.window, cfg.dataset.horizon)
+
+        # Dynamic
+        x = np.stack(loader.features)[:, :, 0, :]
+        y = np.stack(loader.targets)
+
+        # Static
+        edge_index = loader.edges
+        edge_weight = loader.edge_weights
+        n_nodes = x.shape[1]
+
+    return x, y, edge_index, edge_weight, n_nodes
 
 def split_dataset(cfg: OmegaConf, dataset):
     assert cfg.dataset.train_size + cfg.dataset.val_size <= 1, "Train size and validation size must sum to no more than 1"
@@ -40,20 +97,11 @@ def split_dataset(cfg: OmegaConf, dataset):
     sizes_abs = sizes_rel * length
     return torch.utils.data.random_split(Subset(dataset, range(length)), sizes_abs)
 
-def create_dataloaders(cfg: OmegaConf):
-    assert cfg.dataset.name == 'METRLA', "Only METRLA dataset is supported currently"
 
-    # Load data
-    ssl._create_default_https_context = ssl._create_unverified_context  # Fix for SSL verification error
-    loader = METRLADatasetLoader(raw_data_dir=os.path.join(os.getcwd(), 'data', 'METRLA'))
-    loader._get_edges_and_weights()
-    loader._generate_task(cfg.dataset.window, cfg.dataset.horizon)
+def create_dataloaders(cfg: OmegaConf, raw_data_dir=os.path.join(os.path.dirname(__file__), '../data')):
+    x, y, edge_index, edge_weight, n_nodes = get_data_raw(cfg, raw_data_dir)
 
-    # Dynamic
-    x = np.stack(loader.features)[:, :, 0, :]
-    y = np.stack(loader.targets)
-
-    dataset = DynamicNodeDataset(x, y)
+    dataset = DynamicNodeFeatureDataset(x, y)
     train_dataset, val_dataset, test_dataset = split_dataset(cfg, dataset)
 
     train_loader = DataLoader(train_dataset, batch_size=cfg.train.batch_size, shuffle=True, num_workers=cfg.dataset.num_workers, drop_last=True, pin_memory=True, persistent_workers=True)
@@ -62,12 +110,7 @@ def create_dataloaders(cfg: OmegaConf):
     test_loader = DataLoader(test_dataset, batch_size=cfg.train.batch_size, shuffle=True, num_workers=cfg.dataset.num_workers, drop_last=True, pin_memory=True, persistent_workers=True) \
         if len(test_dataset) > 0 else None
 
-    # Static
-    edges = loader.edges
-    edge_weights = loader.edge_weights
-    num_nodes = x.shape[1]
-
-    topo_data = StaticGraphTopologyData(edges, edge_weights, num_nodes)
+    topo_data = StaticGraphTopologyData(edge_index, edge_weight, n_nodes)
     # pre_transform = PositionalEncodingTransform(rw_dim=cfg.pos_enc.rw_dim, lap_dim=cfg.pos_enc.lap_dim)
     if cfg.metis.n_patches > 0:
         transform_train = GraphPartitionTransform(n_patches=cfg.metis.n_patches,
